f2663c910078d27df5aa2543397fcc8be2fefb1c
Uygur Tepe <uygur.t10@gmail.com>
2021-01-12 11:39:52 -0500

    added setViewpoint

--- routes/pdf.js
+++ routes/pdf.js
@@ -7,6 +7,7 @@ router.get('/pdf', async (req, res) => {
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.setViewport({width: 1920, height: 1080})
     await page.goto(link, { waitUntil: 'networkidle0' });
     
     const pdf = await page.pdf({ 

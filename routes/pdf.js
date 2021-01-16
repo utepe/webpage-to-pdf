eb9f610da3300c4fa9ea368fae592f20f84268ea
utepe <uygur.t10@gmail.com>
2021-01-16 16:49:56 -0500

    added executable path for runnin on pi

--- routes/pdf.js
+++ routes/pdf.js
@@ -5,22 +5,24 @@ const router = express.Router();
 router.get('/pdf', async (req, res) => {
     const link = req.query.link;
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        executablePath: '/usr/bin/chromium-browser'
+    });
     const page = await browser.newPage();
-    await page.setViewport({width: 1920, height: 1080})
+    await page.setViewport({ width: 1920, height: 1080 })
     await page.goto(link, { waitUntil: 'networkidle0' });
-    
-    const pdf = await page.pdf({ 
-        printBackground: true, 
+
+    const pdf = await page.pdf({
+        printBackground: true,
         format: 'Letter',
         margin: {
             top: '20px',
             bottom: '40px',
             left: '20px',
             right: '20px'
-        } 
+        }
     });
-    
+
     await browser.close();
 
     res.contentType('application/pdf');
